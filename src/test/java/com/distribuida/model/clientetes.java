da7378440944c4f9e81d301232f28754e6fac277
scriollo <scriollo@tecnologicoismac.edu.ec>
2026-05-21 04:18:01 +0200

    mi primer versionamiento

---
+++ src/test/java/com/distribuida/model/clientetes.java
@@ -0,0 +1,71 @@
+//package com.distribuida.model;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//public class clientetes
+//private Cliente cliente;
+//public void sed UP(){
+//   package com.distribuida.model;
+//   import org.junit.jupiter.api.BeforeEach;
+//   public class clientetes {
+//   private Cliente cliente;
+//   @BeforeEach
+//       public void sed UP(){
+//       cliente = new Cliente(
+//               idCliente:1
+//                   ,cedula :"1719636167"
+//                   ,nombre:"Bob"
+//                   ,apellido:"esponja"
+//                   ,direccion:"en Quito"
+//                   ,telefono:"0992716101"
+//                   ,correoo:"davi1985fabian@gmail.com")
+//       };
+//
+//
+//
+//   }
+//@Test
+//        public void testclienteConstructor
+//     asserTEquall(heading: "Pruebas unitarias - Constructor y Getters(){
+//    () -> assertEquals(expected: 1, cliente.getIdCliente()),
+//    () -> assertEquals(expected: 1, cliente.getIdCliente()),
+//    () -> assertEquals(expected: 1, cliente.getIdCliente()),
+//    () -> assertEquals(expected: 1, cliente.getIdCliente()),
+//    () -> assertEquals(expected: 1, cliente.getIdCliente()),
+//    () -> assertEquals(expected: 1, cliente.getIdCliente()),
+//    () -> assertEquals(expected: 1, cliente.getIdCliente()),
+//    );
+//   System.println(cliente.toString());
+//   );
+//
+//
+//}
+//@Test
+//public void testClienteSetters(){
+//    cliente.setIdCliente(2);
+//    cliente.setCedula("1719636167");
+//    cliente.sedNonbre("Bob22");
+//    cliente.sedApellido("Esponja222");
+//    cliente.setDireccion("Quito");
+//    cliente.setTelefono("0992716101");
+//    );
+//Sistem.out.println(cliente.toString());
+//
+//}
+//@Test
+//public void tesClienteTostring(){
+//
+//   String str = cliente.toString(){
+//       assertAll( heading: "pruebas unitarias - To string - Cliente",
+//                ()-> assertrue(str.contains("1")),
+//                ()-> assertrue(str.contains("1")),
+//                ()-> assertrue(str.contains("1")),
+//                ()-> assertrue(str.contains("1")),
+//                ()-> assertrue(str.contains("1")),
+//                ()-> assertrue(str.contains("1")),
+//    }
+
+
+
+//}
